ed5f9df14c41d76e90fbbea9692cd21c4031948b
gitworkflows <118260833+gitworkflows@users.noreply.github.com>
2024-09-07 07:40:51 +0600

    Update __init__.py

--- bengalinlp/__init__.py
+++ bengalinlp/__init__.py
@@ -1,3 +1,29 @@
-__version__ = "0.0.1"
 
-from .bengalinlp import *
+__version__ = "1.0.0"
+
+import os
+from bengalinlp.tokenizer.basic import BasicTokenizer
+from bengalinlp.tokenizer.nltk import NLTKTokenizer
+from bengalinlp.tokenizer.sentencepiece import (
+    SentencepieceTokenizer, 
+    SentencepieceTrainer,
+)
+
+from bengalinlp.embedding.word2vec import (
+    BengaliWord2Vec,
+    Word2VecTraining,
+)
+from bengalinlp.embedding.glove import BengaliGlove
+
+from bengalinlp.embedding.doc2vec import (
+    BengaliDoc2vec, 
+    BengaliDoc2vecTrainer,
+)
+
+from bengalinlp.token_classification.pos import BengaliPOS
+from bengalinlp.token_classification.ner import BengaliNER
+from bengalinlp.token_classification.token_classification_trainer import CRFTaggerTrainer
+
+from bengalinlp.cleantext.clean import CleanText
+
+from bengalinlp.corpus.corpus import BengaliCorpus
